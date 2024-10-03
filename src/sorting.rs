a815580342e1b582f8ccc2b73baa28d6b55ffe07
quintanamo <quintin@quintinherb.net>
2024-10-03 13:48:43 -0400

    clean up insertion sort and add tests

--- src/sorting.rs
+++ src/sorting.rs
@@ -1,15 +1,52 @@
 pub fn insertion_sort(input: Vec<i32>) -> Vec<i32> {
     let mut sorted: Vec<i32> = input;
 
-    for j in 1..sorted.len() as i32 {
-        let key: i32 = sorted[j as usize];
-        let mut i: i32 = j - 1;
-        while i >= 0 && sorted[i as usize] > key {
-            sorted[(i + 1) as usize] = sorted[i as usize];
+    for j in 1..sorted.len() {
+        let key: i32 = sorted[j];
+        let mut i: usize = j;
+        while i > 0 && sorted[i - 1] > key {
+            sorted[i] = sorted[i - 1];
             i = i - 1;
         }
-        sorted[(i + 1) as usize] = key;
+        sorted[i] = key;
     }
 
     return sorted;
+}
+
+fn is_sorted(input: Vec<i32>) -> bool {
+    let mut last_num = 0;
+    for num in input {
+        if num < last_num {
+            return false;
+        }
+        last_num = num;
+    }
+    return true;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::insertion_sort;
+    use super::is_sorted;
+
+    #[test]
+    fn test_is_sorted() {
+        let mut input: Vec<i32> = [5,2,3,1,4].to_vec();
+        assert_eq!(is_sorted(input), false);
+        input = [1, 2, 3, 4, 5].to_vec();
+        assert_eq!(is_sorted(input), true);
+        input = [1, 1, 2, 2, 3].to_vec();
+        assert_eq!(is_sorted(input), true);
+    }
+
+    #[test]
+    fn test_insertion_sort() {
+        let mut input: Vec<i32> = [5,2,3,1,4].to_vec();
+        let mut output: Vec<i32> = insertion_sort(input);
+        assert_eq!(is_sorted(output), true);
+        input = [1, 1, 2, 1, 1].to_vec();
+        output = insertion_sort(input);
+        assert_eq!(is_sorted(output), true);
+    }
 }
